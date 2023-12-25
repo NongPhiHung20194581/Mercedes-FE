7f2fbd3359ec4a2aa2fe84040d7b543d3539eee8
baoagile <ngoc.hb@agiletechsoftware.com>
2023-12-25 18:29:59 +0700

    update background

--- src/container/ProfileContainer.jsx
+++ src/container/ProfileContainer.jsx
@@ -1,25 +1,24 @@
-import React, { useEffect, useState } from 'react'
-import Profile from '../components/Profile'
-import { useDispatch, useSelector } from 'react-redux'
-import { authSelector, profileSelector } from '../redux/selector'
-import { useFormik } from 'formik'
-import { offDualRingLoading, onDualRingLoading } from '../redux/slices/loading.slice'
-import { validateProfile } from '../validation'
-import { updateProfileUserApi } from '../api/profile.api'
-import Swal from 'sweetalert2'
-import { updateUserInfo } from '../redux/slices/auth.slice'
-import { nationalities } from '../constants/nationality'
-import { changeIsUpdate, saveProfileInfo } from '../redux/slices/profile.slice'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import Profile from '../components/Profile';
+import { useDispatch, useSelector } from 'react-redux';
+import { authSelector, profileSelector } from '../redux/selector';
+import { useFormik } from 'formik';
+import { offDualRingLoading, onDualRingLoading } from '../redux/slices/loading.slice';
+import { validateProfile } from '../validation';
+import { updateProfileUserApi } from '../api/profile.api';
+import Swal from 'sweetalert2';
+import { updateUserInfo } from '../redux/slices/auth.slice';
+import { nationalities } from '../constants/nationality';
+import { changeIsUpdate, saveProfileInfo } from '../redux/slices/profile.slice';
+import { useNavigate } from 'react-router-dom';
 
 const ProfileContainer = () => {
-
-    const { userId } = useSelector(authSelector);
+    const { userId, avatar } = useSelector(authSelector);
     const { data } = useSelector(profileSelector);
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const [message, setMessage] = useState('');
-    const [nationHeight, setNationHeight] = useState(0)
+    const [nationHeight, setNationHeight] = useState(0);
 
     const formik = useFormik({
         initialValues: {
@@ -29,10 +28,10 @@ const ProfileContainer = () => {
             gender: '',
             address: '',
             want_to: '',
-            password: ''
+            password: '',
         },
         validationSchema: validateProfile,
-        onSubmit: values => {
+        onSubmit: (values) => {
             dispatch(onDualRingLoading());
             if (userId) {
                 const form = {
@@ -42,50 +41,50 @@ const ProfileContainer = () => {
                         name: values.name,
                         nationality: values.nationality,
                         phone: values.phone,
-                        wantTo: values.want_to
+                        wantTo: values.want_to,
                     },
-                    password: values.password
+                    password: values.password,
                 };
 
                 if (form.password.trim().length < 1) {
-                    delete form["password"]
+                    delete form['password'];
                 }
 
                 updateProfileUserApi(userId, form)
-                    .then(res => {
+                    .then((res) => {
                         const { user_info } = res.data.result;
                         dispatch(updateUserInfo({ fullName: user_info.name }));
-                        dispatch(saveProfileInfo(user_info))
-                        dispatch(offDualRingLoading())
-                        setMessage(message => message + 'success')
+                        dispatch(saveProfileInfo(user_info));
+                        dispatch(offDualRingLoading());
+                        setMessage((message) => message + 'success');
 
                         Swal.fire({
                             title: 'Success',
-                            text: "プロフィールの更新に成功しました！",
+                            text: 'プロフィールの更新に成功しました！',
                             icon: 'success',
-                            confirmButtonText: 'Home Page'
-                        }).then(result => {
+                            confirmButtonText: 'Home Page',
+                        }).then((result) => {
                             if (result.isConfirmed) {
                                 dispatch(changeIsUpdate(true));
-                                navigate("/")
+                                navigate('/');
                             }
-                        })
+                        });
                     })
-                    .catch(err => {
-                        dispatch(offDualRingLoading())
-                        console.log(err)
-                    })
+                    .catch((err) => {
+                        dispatch(offDualRingLoading());
+                        console.log(err);
+                    });
             }
-        }
-    })
+        },
+    });
 
     const handleSelectNationality = (nationality_id) => {
-        const nationality = nationalities.find(item => item.id === nationality_id);
+        const nationality = nationalities.find((item) => item.id === nationality_id);
         if (nationality) {
-            formik.setFieldValue("nationality", nationality.value);
+            formik.setFieldValue('nationality', nationality.value);
             setNationHeight(0);
         }
-    }
+    };
 
     useEffect(() => {
         const user_info = data;
@@ -97,7 +96,7 @@ const ProfileContainer = () => {
         }
         formik.setValues(formData);
         //eslint-disable-next-line
-    }, [message, data])
+    }, [message, data]);
 
     return (
         <Profile
@@ -105,8 +104,9 @@ const ProfileContainer = () => {
             handleSelectNationality={handleSelectNationality}
             nationHeight={nationHeight}
             setNationHeight={setNationHeight}
+            avatar={avatar}
         />
-    )
-}
+    );
+};
 
-export default ProfileContainer
+export default ProfileContainer;
