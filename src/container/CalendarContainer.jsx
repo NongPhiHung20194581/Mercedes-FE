ee451d43006104c5fda89462dfbb55b087c9f90b
baoagile <ngoc.hb@agiletechsoftware.com>
2023-12-25 16:45:31 +0700

    fix calendar

--- src/container/CalendarContainer.jsx
+++ src/container/CalendarContainer.jsx
@@ -12,31 +12,6 @@ import { toast } from 'react-toastify';
 
 const CalendarContainer = () => {
     const { data } = useSelector(profileSelector);
-    const { userId } = useSelector(authSelector);
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        if (!data) {
-            getProfileForUser(userId)
-                .then((res) => {
-                    const user_info = res.data.result;
-                    dispatch(updateUserInfo({ fullName: user_info.name }));
-                    dispatch(saveProfileInfo(user_info));
-                })
-                .catch((error) => {
-                    toast.error(`Không thể lấy thông tin người dùng! 👻`, {
-                        position: 'bottom-right',
-                        autoClose: 4000,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        pauseOnHover: true,
-                        draggable: true,
-                        progress: undefined,
-                        theme: 'light',
-                    });
-                });
-        }
-    }, [data]);
 
     return data ? (
         <Calendar data={data} classes={dummyScheduleData.classes} schedule={dummyScheduleData.schedule} />
